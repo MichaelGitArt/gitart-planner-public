4b8954abfce0db9abb5da577960037304d0cd722
Michael <michaelgitart@gmail.com>
2020-04-05 19:23:43 +0300

    Enable history mode for app

--- web-server.js
+++ web-server.js
@@ -3,6 +3,8 @@ const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 const express = require('express');
 const { join } = require("path");
+const history = require('connect-history-api-fallback');
+
 
 const app = express();
 
@@ -15,6 +17,17 @@ app.use(helmet());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser());
+
+// Setup server for vue spa
+app.use(history({
+	rewrites: [{
+		from: /^\/api\/.*$/,
+		to: function (context) {
+			return context.parsedUrl.pathname;
+			// return '/dist' + context.parsedUrl.pathname;
+		}
+	}]
+}));
 
 // Allow vue dist folder
 app.use(express.static(join(__dirname, "dist")));
