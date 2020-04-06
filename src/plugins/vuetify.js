f241e214fbe3ce0f4a0d33898658ac0e6561b7a8
Michael <michaelgitart@gmail.com>
2020-04-06 11:29:25 +0300

    Change breakpoint to bootstrap version

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,14 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
-});
+	breakpoint: {
+		thresholds: {
+			xs: 0,
+			sm: 576,
+			md: 768,
+			lg: 992,
+			xl: 1200,
+		},
+		scrollBarWidth: 24,
+	},
+})
