4d2190a4c7fd1db3542725ec9f9b05001f0da9eb
Michael Gitart <michaelgitart@gmail.com>
2020-04-24 21:43:59 +0300

    Update alert component

--- src/components/General/ContentAlert.vue
+++ src/components/General/ContentAlert.vue
@@ -2,24 +2,38 @@
   <transition name="fade" mode="out-in">
     <v-alert
       class="mt-3 body-2"
-      border="bottom"
       colored-border
-      type="warning"
+      :type="type"
       :icon="false"
       dense
+      border="left"
       elevation="2"
-      v-if="message"
-    >{{message}}</v-alert>
+      v-if="value"
+    >
+      <v-row align="center" no-gutters>
+        <v-col class="grow">{{value}}</v-col>
+        <v-spacer></v-spacer>
+        <v-col class="shrink">
+          <v-btn @click="reset" color="success" outlined>OK</v-btn>
+        </v-col>
+      </v-row>
+    </v-alert>
   </transition>
 </template>
 
 <script>
 export default {
   props: {
-    message: [String, null],
+    value: [String, null],
     type: {
       type: String,
       default: "warning"
+    }
+  },
+  methods: {
+    reset() {
+      console.log("rest");
+      this.$emit("input", "");
     }
   }
 };
