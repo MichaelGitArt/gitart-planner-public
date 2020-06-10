cb488b6635ba61932b4479f356dba0b6c1233455
Michael Gitart <michaelgitart@gmail.com>
2020-06-10 13:26:54 +0300

    Change base components import

---
+++ src/components/Base/index.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+
+import BaseContentCard from '@/components/Base/BaseContentCard';
+import BasePageBack from '@/components/Base/BasePageBack';
+
+Vue.component('BaseContentCard', BaseContentCard);
+Vue.component('BasePageBack', BasePageBack);
--- src/main.js
+++ src/main.js
@@ -1,58 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import { upperFirst, camelCase } from 'lodash'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
 import vuetify from './plugins/vuetify';
 import userExpration from './plugins/user-expantion';
 import '@/utils/VeeValidate.js';
 
+// Import base components
+import '@/components/Base';
+
 import '@/scss/main.scss';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 Vue.use(userExpration);
-
-
-const requireComponent = require.context(
-	// Относительный путь до каталога компонентов
-	'./components/',
-	// Обрабатывать или нет подкаталоги
-	true,
-	// Регулярное выражение для определения файлов базовых компонентов
-	/Base[A-Z]\w+\.(vue|js)$/
-)
-
-requireComponent.keys().forEach(fileName => {
-	// Получение конфигурации компонента
-	const componentConfig = requireComponent(fileName)
-
-	// Получение имени компонента в PascalCase
-	const componentName = upperFirst(
-		camelCase(
-			// Получаем имя файла независимо от глубины вложенности
-			fileName
-				.split('/')
-				.pop()
-				.replace(/\.\w+$/, '')
-		)
-	)
-
-	// Глобальная регистрация компонента
-	Vue.component(
-		componentName,
-		// Поиск опций компонента в `.default`, который будет существовать,
-		// если компонент экспортирован с помощью `export default`,
-		// иначе будет использован корневой уровень модуля.
-		componentConfig.default || componentConfig
-	)
-})
-
 
 new Vue({
 	router,
 	store,
 	vuetify,
-	render: h => h(App)
-}).$mount('#app')
+	render: (h) => h(App),
+}).$mount('#app');
